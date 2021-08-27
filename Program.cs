b1ac8cce0eca71897c41beadfe2079e5ba9c9683
Daslaw <64631869+Daslaw@users.noreply.github.com>
2021-08-27 13:30:28 +0100

    Second Ass

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace subtraction2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration of Values
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int subResult= 0;
+
+            // Request for values from the respondent
+            Console.WriteLine("Enter your first Number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Second Number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+           // Subtraction and Displays of Result
+            subResult = firstNumber - secondNumber;  
+            Console.WriteLine("The result is: "+ subResult);
+        }
+    }
+}
